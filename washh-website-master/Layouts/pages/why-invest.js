95ffc163c7ebd8a231a17bc8cc8d7d63a352d4db
Chitraranjan Gogoi <chitraranjan@Chitraranjans-MacBook-Air.local>
2022-04-05 18:42:34 +0530

    Franshise page UI changes

--- washh-website-master/Layouts/pages/why-invest.js
+++ washh-website-master/Layouts/pages/why-invest.js
@@ -7,7 +7,7 @@ document.write(`
 		<br/><br/>
 		<div class="flex-container-invest">
 			<div class="flex-container-col">
-				<img src="" alt="" class="img-invest"/>
+				<img src="./images/Franchise-1.png" alt="" class="img-invest"/>
 				<h4 class="subhead-invest">FOCO Model</h4>
 				<p class="text-invest">
 					Having successfully operated in the online laundry business since 2017, we have tried out different formats of fulfillment to understand what works best. Through the Franchise Owned Company Operated, we take care of all your Franchise needs, including training and supervising your staff. Hence, you have to do very little in terms of everyday operations. This ensures not only a hassle-free customer experience but also an effective franchise onboarding and operational experience.	
@@ -15,7 +15,7 @@ document.write(`
 			</div>
 			<div class="flex-container-colM"></div>
 			<div class="flex-container-col">
-				<img src="" alt="" class="img-invest"/>
+			<img src="./images/Franchise-2.png" alt="" class="img-invest"/>
 				<h4 class="subhead-invest">Smart Franchise Dashboard</h4>
 				<p class="text-invest">					
 					Managing multiple businesses can become challenging. However, with Washh Smart Franchise Dashboard, you can track your business performance day to day from anywhere in the world. The cloud-based dashboard assists with real-time monitoring of day-to-day sales, orders, sales, revenues, and more. 
@@ -25,7 +25,7 @@ document.write(`
 		<br/><br/>
 		<div class="flex-container-invest">
 			<div class="flex-container-col">
-				<img src="" alt="" class="img-invest"/>
+			<img src="./images/Franchise-3.png" alt="" class="img-invest"/>
 				<h4 class="subhead-invest"> Store Monitoring System powered by AI</h4>
 				<p class="text-invest">
 					Our store monitoring system is enhanced with AI and is craftily designed to ensure complete security and alert the franchise owners in case of any issues. This ensures you have full control of your business anytime. 	
@@ -33,7 +33,7 @@ document.write(`
 			</div>
 			<div class="flex-container-colM"></div>
 			<div class="flex-container-col">
-				<img src="" alt="" class="img-invest"/>
+			<img src="./images/Franchise-4.png" alt="" class="img-invest"/>
 				<h4 class="subhead-invest">360&deg; Marketing Support</h4>
 				<p class="text-invest">
 					We understand that marketing is an integral part of generating business for a franchise. WASHH has its in-house dedicated team which will implement marketing strategies via Social media, Digital marketing, off-line, on-store to create a buzz around your establishment and bring your target customers closer to you.	
